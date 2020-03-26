2cb82b64dcfb2d98a0489e7a357c8a903efd8724
Botezatu <57545119+andrei747@users.noreply.github.com>
2020-03-26 20:07:43 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Cupcakes.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Cupcakes.Repositories;
+
+namespace Cupcakes
+{
+	public class Startup
+	{
+		private IConfiguration _configuration;
+		public Startup(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddTransient<ICupcakeRepository, CupcakeRepository>();
+			services.AddDbContext<CupcakeContext>(options =>
+				options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+			services.AddHttpContextAccessor();
+			services.AddControllersWithViews();
+			services.AddRazorPages();
+		}
+		public void Configure(IApplicationBuilder app, CupcakeContext cupcakeContext)
+		{
+			cupcakeContext.Database.EnsureDeleted();///error
+			cupcakeContext.Database.EnsureCreated();
+			app.UseStaticFiles();
+			app.UseRouting();
+			app.UseEndpoints(endpoints => {
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Cupcake}/{action=Index}/{id?}");
+				endpoints.MapRazorPages();
+			});
+		}
+	}
+
+
+}
