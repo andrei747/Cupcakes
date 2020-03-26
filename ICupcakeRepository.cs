84f08e27528fa619dabfdf33cbac62ce790cd5d7
Botezatu <57545119+andrei747@users.noreply.github.com>
2020-03-26 20:10:43 +0200

    Add files via upload

---
+++ ICupcakeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cupcakes.Models;
+using Cupcakes.Repositories;
+
+namespace Cupcakes.Repositories
+{
+    public interface ICupcakeRepository
+    {
+        IEnumerable<Cupcake> GetCupcakes();
+        Cupcake GetCupcakeById(int id);
+        void CreateCupcake(Cupcake cupcake);
+        void DeleteCupcake(int id);
+        void SaveChanges();
+        IQueryable<Bakery> PopulateBakeriesDropDownList();
+
+
+    }
+}
